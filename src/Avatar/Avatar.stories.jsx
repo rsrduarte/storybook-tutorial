e79946d532380a8473cb0086874a3c09786b211d
Rute Duarte <rute.duarte@enghouse.com>
2024-03-22 11:53:45 +0000

    first commit

---
+++ src/Avatar/Avatar.stories.jsx
@@ -0,0 +1,82 @@
+import { Avatar } from './Avatar';
+
+export default {
+  title: 'Design System/Avatar',
+  component: Avatar,
+};
+
+export const Standard = {
+  args: {
+    size: 'large',
+    username: 'Tom Coleman',
+    src: 'https://avatars2.githubusercontent.com/u/132554',
+  },
+};
+
+/*
+ * New story using Controls
+ * Read more about Storybook templates at:
+ * https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+ */
+export const Controls = {
+  args: {
+    loading: false,
+    size: 'tiny',
+    username: 'Dominic Nguyen',
+    src: 'https://avatars.githubusercontent.com/u/263385',
+  },
+};
+
+export const Sizes = {
+  args: {
+    username: 'Tom Coleman',
+    src: 'https://avatars2.githubusercontent.com/u/132554',
+  },
+  render: (args) => (
+    <>
+      <Avatar {...args} size='large' />
+      <Avatar {...args} size='medium' />
+      <Avatar {...args} size='small' />
+      <Avatar {...args} size='tiny' />
+    </>
+  ),
+};
+
+export const Initials = {
+  render: (args) => (
+    <>
+      <Avatar username='Tom Coleman' />
+      <Avatar username='Dominic Nguyen' />
+      <Avatar username='Varun Vachhar' />
+      <Avatar username='Michael Shilman' />
+    </>
+  ),
+};
+
+export const Loading = {
+  args: {
+    loading: true,
+  },
+  render: (args) => (
+    <>
+      <Avatar {...args} size='large' />
+      <Avatar {...args} size='medium' />
+      <Avatar {...args} size='small' />
+      <Avatar {...args} size='tiny' />
+    </>
+  ),
+};
+
+export const Large = {
+  render: () => (
+    <>
+      <Avatar loading size='large' />
+      <Avatar size='large' username='Tom Coleman' />
+      <Avatar
+        size='large'
+        username='Tom Coleman'
+        src='https://avatars2.githubusercontent.com/u/132554'
+      />
+    </>
+  ),
+};
